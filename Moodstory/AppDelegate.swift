62426ff48c9b6e3bad9a2ac299722bab06d440d3
Muhammad Mustafa Saeed <mmsaeed@kth.se>
2017-03-14 18:23:46 +0100

    Facebook sign in functionality implemented

--- Moodstory/AppDelegate.swift
+++ Moodstory/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+
 
 
 @UIApplicationMain
@@ -19,7 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // Firebase Configurations
          FIRApp.configure()
+        
+        // Facebook Api Configuration for Single Sign-On
+         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         
         return true
     }
@@ -45,6 +52,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+ 
 
 
 }
--- Moodstory/MSLoginSignUpController.swift
+++ Moodstory/MSLoginSignUpController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 import Firebase
+
 
 class MSLoginSignUpController: UIViewController {
 
@@ -21,6 +24,46 @@ class MSLoginSignUpController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        // Authenticate with Facebook
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil{
+                print("Unable to authenticate with Facebook")
+            }
+            else if result?.isCancelled == true {
+                
+                print("User Cancelled facebook Authentication")
+            }
+            else
+            {
+                print("Successfully Authenticated with Facebook")
+                let credentials = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credentials)
+            }
+            
+        }
+        
+    }
+    
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil
+            {
+                print("Unable to authenticate with Firebase - \(String(describing: error))")
+            }
+            else
+            {
+                print("Successfully Authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
