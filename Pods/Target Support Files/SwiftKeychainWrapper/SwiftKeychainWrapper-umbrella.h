1b13e349869c03107eada05a70a977cb85a642f7
Mohit Ahuja <mohit@houzify.com>
2017-03-18 02:58:30 +0100

    my pods

--- Pods/Target Support Files/SwiftKeychainWrapper/SwiftKeychainWrapper-umbrella.h
+++ Pods/Target Support Files/SwiftKeychainWrapper/SwiftKeychainWrapper-umbrella.h
@@ -10,7 +10,8 @@
 #endif
 #endif
 
+#import "SwiftKeychainWrapper.h"
 
-FOUNDATION_EXPORT double Pods_MoodstoryVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MoodstoryVersionString[];
+FOUNDATION_EXPORT double SwiftKeychainWrapperVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftKeychainWrapperVersionString[];
 
