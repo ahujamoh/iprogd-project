9c5e67c7536c8685a5e7eb5610cfb1926984dcbb
Mohit Ahuja <mohit@houzify.com>
2017-03-18 04:55:26 +0100

    location with marker

--- Moodstory/MSCameraViewController.swift
+++ Moodstory/MSCameraViewController.swift
@@ -18,11 +18,18 @@ class MSCameraViewController: UIViewController {
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     
     override func loadView() {
-        //currently setting just the location to singaport
-        let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
-        //putting the view on the place on the map
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-        self.view = mapView
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
     
     override func viewDidLoad() {
