abe63dbeac27eb6c85ab366c241afb9f7dffa3f6
Muhammad Mustafa Saeed <mmsaeed@kth.se>
2017-03-18 03:32:18 +0100

    logout from menu slider , image and name placeholder added

--- Moodstory/MSMenuViewController.swift
+++ Moodstory/MSMenuViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
-class MSMenuViewController: UITableViewController {
+class MSMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    @IBOutlet weak var menuTableView: UITableView!
+    @IBOutlet weak var ProfilePicture: UIImageView!
+    @IBOutlet weak var nameOfUser: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        
+        ProfilePicture.layer.borderWidth = 1
+        ProfilePicture.layer.masksToBounds = false
+        ProfilePicture.layer.borderColor = UIColor.white.cgColor
+        ProfilePicture.layer.cornerRadius = ProfilePicture.frame.height/2
+        ProfilePicture.clipsToBounds = true
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +34,31 @@ class MSMenuViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+     // MARK: - Table view data source
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "Sign Out"
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+        print("ID removed from keyhain : \(keychainResult)")
+        try! FIRAuth.auth()?.signOut()
+        performSegue(withIdentifier: "goToLoginScreen", sender: nil)
+        
     }
     
 
