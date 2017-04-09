0b32f1e51a2c14fd008c3dfd9a681eac925ddf04
Mohit Ahuja <mohit@houzify.com>
2017-04-09 19:34:33 +0200

    adding chat viewcontroller

---
+++ Moodstory/MSChatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MSChatViewController.swift
+//  Moodstory
+//
+//  Created by Mohit on 09/04/17.
+//  Copyright © 2017 MoodStory. All rights reserved.
+//
+
+import UIKit
+
+class MSChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
