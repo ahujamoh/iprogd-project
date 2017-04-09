88101f2c61f56236a3ce7b2a0d000e2d65e628bf
Mohit Ahuja <mohit@houzify.com>
2017-04-09 19:27:26 +0200

    basic code

--- Moodstory/MSConversationViewController.swift
+++ Moodstory/MSConversationViewController.swift
@@ -8,9 +8,32 @@
 
 import UIKit
 
-class MSConversationViewController: UIViewController {
+class MSConversationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items = [Conversation]()
+    var selectedUser: User?
+    
+    func fetchData(){
+        Conversation.showConversations { (conversations) in
+            self.items = conversations
+            self.items.sort{ $0.lastMessage.timestamp > $1.lastMessage.timestamp }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                for conversation in self.items {
+                    if conversation.lastMessage.isRead == false {
+//                        self.playSound()
+                        break
+                    }
+                }
+            }
+        }
+    }
+    
+    //MARK: View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +47,7 @@ class MSConversationViewController: UIViewController {
             
         }
         
+        self.fetchData()
         // Do any additional setup after loading the view.
     }
 
