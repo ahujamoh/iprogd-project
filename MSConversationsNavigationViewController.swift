a038d10a26b00976fb11e37fcf635cc30e09c312
Mohit Ahuja <mohit@houzify.com>
2017-04-10 19:10:10 +0200

    adding changes to add a view which loads and ha the option to show all the contacts

--- MSConversationsNavigationViewController.swift
+++ MSConversationsNavigationViewController.swift
@@ -7,29 +7,167 @@
 //
 
 import UIKit
-
-class MSConversationsNavigationViewController: UINavigationController {
-
+import Firebase
+class MSConversationsNavigationViewController: UINavigationController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionsView: UICollectionView!
+    @IBOutlet var contactsView: UIView!
+    let darkView = UIView.init()
+    var items = [User]()
+    var topAnchorContraint: NSLayoutConstraint!
+    
+    //MARK: methods to show and hide the view
+    func customization(){
+        //DarkView customization
+        self.view.addSubview(self.darkView)
+        self.darkView.backgroundColor = UIColor.black
+        self.darkView.alpha = 0
+        self.darkView.translatesAutoresizingMaskIntoConstraints = false
+        self.darkView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.darkView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.darkView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.darkView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.darkView.isHidden = true
+        //ContainerView customization
+        let extraViewsContainer = UIView.init()
+        extraViewsContainer.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(extraViewsContainer)
+        self.topAnchorContraint = NSLayoutConstraint.init(item: extraViewsContainer, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 1000)
+        self.topAnchorContraint.isActive = true
+        extraViewsContainer.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        extraViewsContainer.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        extraViewsContainer.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        extraViewsContainer.backgroundColor = UIColor.clear
+        //ContactsView customization
+        extraViewsContainer.addSubview(self.contactsView)
+        self.contactsView.translatesAutoresizingMaskIntoConstraints = false
+        self.contactsView.topAnchor.constraint(equalTo: extraViewsContainer.topAnchor).isActive = true
+        self.contactsView.leadingAnchor.constraint(equalTo: extraViewsContainer.leadingAnchor).isActive = true
+        self.contactsView.trailingAnchor.constraint(equalTo: extraViewsContainer.trailingAnchor).isActive = true
+        self.contactsView.bottomAnchor.constraint(equalTo: extraViewsContainer.bottomAnchor).isActive = true
+        self.contactsView.isHidden = true
+        self.collectionsView?.contentInset = UIEdgeInsetsMake(10, 0, 0, 0)
+        self.contactsView.backgroundColor = UIColor.clear
+        //NotificationCenter for showing extra views
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showExtraViews(notification:)), name: NSNotification.Name(rawValue: "showExtraView"), object: nil)
+        self.fetchUsers()
+    }
+    
+    func dismissExtraViews(){
+        self.topAnchorContraint.constant = 1000
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+            self.darkView.alpha = 0
+            self.view.transform = CGAffineTransform.identity
+        }, completion:  { (true) in
+            self.darkView.isHidden = true
+            self.contactsView.isHidden = true
+            let vc = self.viewControllers.last
+            vc?.inputAccessoryView?.isHidden = false
+        })
+    }
+    
+    //Show extra view
+    func showExtraViews(notification: NSNotification)  {
+        let transform = CGAffineTransform.init(scaleX: 0.94, y: 0.94)
+        self.topAnchorContraint.constant = 0
+        self.darkView.isHidden = false
+        if let type = notification.userInfo?["viewType"] as? ShowExtraView {
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+                self.darkView.alpha = 0.8
+                if (type == .contacts || type == .profile) {
+                    self.view.transform = transform
+                }
+            })
+            switch type {
+            case .contacts:
+                self.contactsView.isHidden = false
+                if self.items.count == 0 {
+                    print("no item")
+                }
+            default:
+                print("you messed up real bad, should not have come here")
+            }
+        }
+    }
+    
+    //Downloads users list for Contacts View
+    func fetchUsers()  {
+        if let id = FIRAuth.auth()?.currentUser?.uid {
+            User.downloadAllUsers(exceptID: id, completion: {(user) in
+                DispatchQueue.main.async {
+                    self.items.append(user)
+                    self.collectionsView.reloadData()
+                }
+            })
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ContactsCVCell
+        cell.profilePic.image = self.items[indexPath.row].profilePic
+        cell.nameLabel.text = self.items[indexPath.row].name
+        cell.profilePic.layer.borderWidth = 2
+        cell.profilePic.layer.borderColor = GlobalVariables.purple.cgColor
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if self.items.count > 0 {
+            self.dismissExtraViews()
+            let userInfo = ["user": self.items[indexPath.row]]
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showUserMessages"), object: nil, userInfo: userInfo)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if self.items.count == 0 {
+            return self.collectionsView.bounds.size
+        } else {
+            if UIScreen.main.bounds.width > UIScreen.main.bounds.height {
+                let width = (0.3 * UIScreen.main.bounds.height)
+                let height = width + 30
+                return CGSize.init(width: width, height: height)
+            } else {
+                let width = (0.3 * UIScreen.main.bounds.width)
+                let height = width + 30
+                return CGSize.init(width: width, height: height)
+            }
+        }
+    }
+    
+    
+    @IBAction func closeButtonClicked(_ sender: Any) {
+        self.dismissExtraViews()
+    }
+    
+    //MARK: delegate methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.customization()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        self.view.transform = CGAffineTransform.identity
     }
-    */
-
+    
 }
--- Moodstory/MSConversationViewController.swift
+++ Moodstory/MSConversationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MSConversationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     
     @IBOutlet weak var tableView: UITableView!
@@ -18,10 +18,14 @@ class MSConversationViewController: UIViewController, UITableViewDelegate, UITab
     var selectedUser: User?
     
     func showContacts(){
+        let info = ["viewType" : ShowExtraView.contacts]
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showExtraView"), object: nil, userInfo: info)
     }
     
     func setCreateConversation(){
-    //right bar button
+        //Notification setup
+        NotificationCenter.default.addObserver(self, selector: #selector(self.pushToUserMesssages(notification:)), name: NSNotification.Name(rawValue: "showUserMessages"), object: nil)
+        //right bar button
         let icon = UIImage.init(named: "send")?.withRenderingMode(.alwaysOriginal)
         let rightButton = UIBarButtonItem.init(image: icon!, style: .plain, target: self, action: #selector(MSConversationViewController.showContacts))
         self.navigationItem.rightBarButtonItem = rightButton
@@ -35,11 +39,19 @@ class MSConversationViewController: UIViewController, UITableViewDelegate, UITab
                 self.tableView.reloadData()
                 for conversation in self.items {
                     if conversation.lastMessage.isRead == false {
-//                        self.playSound()
+                        //                        self.playSound()
                         break
                     }
                 }
             }
+        }
+    }
+    
+    //Shows Chat viewcontroller with given user
+    func pushToUserMesssages(notification: NSNotification) {
+        if let user = notification.userInfo?["user"] as? User {
+            self.selectedUser = user
+            self.performSegue(withIdentifier: "segueToChat", sender: self)
         }
     }
     
@@ -71,38 +83,38 @@ class MSConversationViewController: UIViewController, UITableViewDelegate, UITab
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ConversationsTBCell
-            cell.clearCellData()
-            cell.profilePic.image = self.items[indexPath.row].user.profilePic
-            cell.nameLabel.text = self.items[indexPath.row].user.name
-            switch self.items[indexPath.row].lastMessage.type {
-            case .text:
-                let message = self.items[indexPath.row].lastMessage.content as! String
-                cell.messageLabel.text = message
-            case .location:
-                cell.messageLabel.text = "Location"
-            default:
-                cell.messageLabel.text = "Media"
-            }
-            let messageDate = Date.init(timeIntervalSince1970: TimeInterval(self.items[indexPath.row].lastMessage.timestamp))
-            let dataformatter = DateFormatter.init()
-            dataformatter.timeStyle = .short
-            let date = dataformatter.string(from: messageDate)
-            cell.timeLabel.text = date
-            if self.items[indexPath.row].lastMessage.owner == .sender && self.items[indexPath.row].lastMessage.isRead == false {
-                cell.nameLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 17.0)
-                cell.messageLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 14.0)
-                cell.timeLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 13.0)
-                cell.profilePic.layer.borderColor = GlobalVariables.blue.cgColor
-                cell.messageLabel.textColor = GlobalVariables.purple
-            }
-            return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ConversationsTBCell
+        cell.clearCellData()
+        cell.profilePic.image = self.items[indexPath.row].user.profilePic
+        cell.nameLabel.text = self.items[indexPath.row].user.name
+        switch self.items[indexPath.row].lastMessage.type {
+        case .text:
+            let message = self.items[indexPath.row].lastMessage.content as! String
+            cell.messageLabel.text = message
+        case .location:
+            cell.messageLabel.text = "Location"
+        default:
+            cell.messageLabel.text = "Media"
+        }
+        let messageDate = Date.init(timeIntervalSince1970: TimeInterval(self.items[indexPath.row].lastMessage.timestamp))
+        let dataformatter = DateFormatter.init()
+        dataformatter.timeStyle = .short
+        let date = dataformatter.string(from: messageDate)
+        cell.timeLabel.text = date
+        if self.items[indexPath.row].lastMessage.owner == .sender && self.items[indexPath.row].lastMessage.isRead == false {
+            cell.nameLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 17.0)
+            cell.messageLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 14.0)
+            cell.timeLabel.font = UIFont(name:"AvenirNext-DemiBold", size: 13.0)
+            cell.profilePic.layer.borderColor = GlobalVariables.blue.cgColor
+            cell.messageLabel.textColor = GlobalVariables.purple
+        }
+        return cell
     }
     
     //MARK: View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         if revealViewController() != nil {
@@ -116,21 +128,21 @@ class MSConversationViewController: UIViewController, UITableViewDelegate, UITab
         self.fetchData()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
