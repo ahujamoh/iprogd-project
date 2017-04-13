a7d54caaccc5e434b0677a594b71f97ab76ee58a
Mohit Ahuja <mohit@houzify.com>
2017-04-13 19:15:41 +0200

    disableing db fetch

--- Moodstory/Conversation.swift
+++ Moodstory/Conversation.swift
@@ -20,21 +20,23 @@ class Conversation {
     class func showConversations(completion: @escaping ([Conversation]) -> Swift.Void) {
         if let currentUserID = FIRAuth.auth()?.currentUser?.uid {
             var conversations = [Conversation]()
-            FIRDatabase.database().reference().child("users").child(currentUserID).child("conversations").observe(.childAdded, with: { (snapshot) in
-                if snapshot.exists() {
-                    let fromID = snapshot.key
-                    let values = snapshot.value as! [String: String]
-                    let location = values["location"]!
-                    User.info(forUserID: fromID, completion: { (user) in
-                        let emptyMessage = Message.init(type: .text, content: "loading", owner: .sender, timestamp: 0, isRead: true)
-                        let conversation = Conversation.init(user: user, lastMessage: emptyMessage)
-                        conversations.append(conversation)
-                        conversation.lastMessage.downloadLastMessage(forLocation: location, completion: { (_) in
-                            completion(conversations)
-                        })
-                    })
-                }
-            })
+            //put some stuff here.
+            completion(conversations)
+//            FIRDatabase.database().reference().child("users").child(currentUserID).child("conversations").observe(.childAdded, with: { (snapshot) in
+//                if snapshot.exists() {
+//                    let fromID = snapshot.key
+//                    let values = snapshot.value as! [String: String]
+//                    let location = values["location"]!
+//                    User.info(forUserID: fromID, completion: { (user) in
+//                        let emptyMessage = Message.init(type: .text, content: "loading", owner: .sender, timestamp: 0, isRead: true)
+//                        let conversation = Conversation.init(user: user, lastMessage: emptyMessage)
+//                        conversations.append(conversation)
+//                        conversation.lastMessage.downloadLastMessage(forLocation: location, completion: { (_) in
+//                            completion(conversations)
+//                        })
+//                    })
+//                }
+//            })
         }
     }
     
