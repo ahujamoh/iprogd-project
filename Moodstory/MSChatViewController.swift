0577db3b33c592ee0be4695dca80477dbffc4335
Mohit Ahuja <mohit@houzify.com>
2017-04-16 11:28:01 +0200

    enabling the ui for the conversation table as well

--- Moodstory/MSChatViewController.swift
+++ Moodstory/MSChatViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class MSChatViewController: UIViewController {
-
+class MSChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
     @IBOutlet var inputBar: UIView!
     @IBOutlet weak var tableView: UITableView!
     
@@ -30,27 +30,125 @@ class MSChatViewController: UIViewController {
         })
         Message.markMessagesRead(forUserID: self.currentUser!.id)
     }
+    
+    func setTemporaryData(){
+        let sendersMessage = Message.init(type: MessageType.text, content: "Sender's Message", owner: MessageOwner.sender, timestamp: 999999999, isRead: true)
+        let receiversMessage = Message.init(type: MessageType.text, content: "Receiver's Message", owner: MessageOwner.receiver, timestamp: 1000000200, isRead: true)
+        items.append(sendersMessage)
+        items.append(receiversMessage)
+    }
+    
     //MARK: Viewcontroller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchData()
+        //        fetchData()
+        setTemporaryData()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if tableView.isDragging {
+            cell.transform = CGAffineTransform.init(scaleX: 0.5, y: 0.5)
+            UIView.animate(withDuration: 0.3, animations: {
+                cell.transform = CGAffineTransform.identity
+            })
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch self.items[indexPath.row].owner {
+        case .receiver:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Receiver", for: indexPath) as! MSReceiverCell
+            cell.clearCellData()
+            switch self.items[indexPath.row].type {
+            case .text:
+                cell.message.text = self.items[indexPath.row].content as! String
+            case .photo:
+                if let image = self.items[indexPath.row].image {
+                    cell.messageBackground.image = image
+                    cell.message.isHidden = true
+                } else {
+                    cell.messageBackground.image = UIImage.init(named: "loading")
+                    self.items[indexPath.row].downloadImage(indexpathRow: indexPath.row, completion: { (state, index) in
+                        if state == true {
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    })
+                }
+            case .location:
+                cell.messageBackground.image = UIImage.init(named: "location")
+                cell.message.isHidden = true
+            }
+            return cell
+        case .sender:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Sender", for: indexPath) as! MSSenderCell
+            cell.clearCellData()
+            cell.profilePic.image = self.currentUser?.profilePic
+            switch self.items[indexPath.row].type {
+            case .text:
+                cell.message.text = self.items[indexPath.row].content as! String
+            case .photo:
+                if let image = self.items[indexPath.row].image {
+                    cell.messageBackground.image = image
+                    cell.message.isHidden = true
+                } else {
+                    cell.messageBackground.image = UIImage.init(named: "loading")
+                    self.items[indexPath.row].downloadImage(indexpathRow: indexPath.row, completion: { (state, index) in
+                        if state == true {
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    })
+                }
+            case .location:
+                cell.messageBackground.image = UIImage.init(named: "location")
+                cell.message.isHidden = true
+            }
+            return cell
+        }
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        self.inputTextField.resignFirstResponder()
+//        switch self.items[indexPath.row].type {
+//        case .photo:
+//            if let photo = self.items[indexPath.row].image {
+//                let info = ["viewType" : ShowExtraView.preview, "pic": photo] as [String : Any]
+//                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showExtraView"), object: nil, userInfo: info)
+//                self.inputAccessoryView?.isHidden = true
+//            }
+//        case .location:
+//            let coordinates = (self.items[indexPath.row].content as! String).components(separatedBy: ":")
+////            let location = nil //CLLocationCoordinate2D.init(latitude: CLLocationDegrees(coordinates[0])!, longitude: CLLocationDegrees(coordinates[1])!)
+//            let info = ["viewType" : ShowExtraView.map, "location": nil] as [String : Any]
+//            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showExtraView"), object: nil, userInfo: info)
+//            self.inputAccessoryView?.isHidden = true
+//        default: break
+//        }
+//    }
 }
