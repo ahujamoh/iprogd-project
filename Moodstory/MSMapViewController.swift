44d7f63c6b7e8160b8538ab9ebdcf73630790018
sarace <Sara@Saras-MacBook-Pro.local>
2017-04-17 11:18:44 +0200

    corelocation framework + get the users location to display on the map

--- Moodstory/MSMapViewController.swift
+++ Moodstory/MSMapViewController.swift
@@ -8,21 +8,52 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MSMapViewController: UIViewController {
+class MSMapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+
+    let manager = CLLocationManager()
+    func locationManager(_manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.15, 0.15)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        mapView.setRegion(region, animated: true)
+        
+        self.mapView.showsUserLocation = true
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
         
+        //self.initialLocation()
         
-            
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
+    //sets initial location to Stockholm
+    func initialLocation() {
+        
+        let location = CLLocationCoordinate2DMake(59.334591, 18.063240)
+        let span = MKCoordinateSpanMake(0.15, 0.15)
+        let region = MKCoordinateRegion(center: location, span: span)
+
+        mapView.setRegion(region, animated: true)
+    
+    }
+    
+    //markers
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
